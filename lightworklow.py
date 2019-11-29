6051e94e4efda3a1c20fda7b5406a2b6257f9a8c
Patryk <patryk.zajac05@gmail.com>
2019-11-29 15:25:40 +0100

    Add XGBoost, Jsonize the model parameters,fixed some errors

--- lightworklow.py
+++ lightworklow.py
@@ -1,8 +1,10 @@
 import lightgbm as lgb 
+import xgboost as xgb
 import pandas as pd 
 import numpy as np 
 import os 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import json 
 from sklearn.model_selection import StratifiedKFold,TimeSeriesSplit,KFold
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,f1_score
@@ -10,58 +12,64 @@ from sklearn.metrics import r2_score,f1_score
 
 class GBM:
     def __init__(self,
-                train_gbm : bool, 
+                train_gbm : bool,
+                train_xg : bool,
                 test_predict : bool, 
                 save_model : bool,
                 save_history : bool,
-                src_dir : str,
                 seed : int,
                 name : str,
-                categorical_features : list,
                 importance : bool,
                 stratify : bool,
                 eval_metric,
                 time_series : bool,
-                prepare_submission : bool): 
-                self.train_gbm = train_gbm 
+                prepare_submission : bool,
+                jsonize : bool): 
+                self.train_gbm = train_gbm
+                self.train_xg = train_xg
                 self.test_predict = test_predict 
                 self.save_model = save_model
                 self.save_history = save_history
-                self.src_dir = src_dir,
                 self.seed = seed
                 self.name = name
                 self.prepare_submission = prepare_submission
                 self.history = {}
-                self.categorical_features = categorical_features
                 self.importance = importance
                 self.stratify = stratify
                 self.eval_metric = eval_metric
                 self.time_series = time_series
+                self.jsonize = jsonize
     
-    def fold_run(self, 
+    def fold_run(self,
+            src_dir : str,
             X_train : pd.DataFrame, 
             y_train,
             X_test : pd.DataFrame,
             n_folds : int,
             parameters = None,
-            categorical_feature = None):
+            categorical_features = None):
 
             if isinstance(y_train, pd.DataFrame):
                 y_train = y_train.values 
 
-            if self.src_dir: 
-                print("Making dir {}".format(self.src_dir))
-                os.makedirs('{}'.format(self.src_dir))
-            
+            if src_dir:
+                if os.path.isdir(src_dir):
+                    pass
+                else:
+                    print(f"Making dir:{src_dir}")
+                    os.makedirs(src_dir)
             try: 
                 print("X_train_shape",X_train.shape)
                 print("X_test_shape",X_test.shape)
             except ValueError: 
                 print("Shape does not fit")
 
-            if self.stratify: 
-                print("Make {} stratify folds".format(n_folds))
-                kf = StratifiedKFold(n_splits=n_folds,random_state = self.seed)
+            if self.stratify:
+                # Bugs...
+                #print("Make {} stratify folds".format(n_folds))
+                #kf = StratifiedKFold(n_splits=n_folds,random_state = self.seed)
+                raise Exception("Bugs... Could not proceed")
+
             elif self.time_series:
                 kf = TimeSeriesSplit(n_splits=n_folds,random_state = self.seed)
             else: 
@@ -74,8 +82,8 @@ class GBM:
             
             i = 0
             for train_index, val_index in kf.split(X_train,y_train):
-                print("Train LightGBM")
-                if self.train_gbm: 
+                if self.train_gbm:
+                    print("Train LightGBM")
                     train_X = X_train.iloc[train_index]
                     val_X = X_train.iloc[val_index]
                     if isinstance(y_train,pd.DataFrame): 
@@ -85,109 +93,134 @@ class GBM:
                         train_y = y_train[train_index]
                         val_y  = y_train[val_index]
                     
-                    lgb_train = lgb.Dataset(train_X,train_y,categorical_feature = self.categorical_features)
-                    lgb_val = lgb.Dataset(val_X,val_y,categorical_feature = self.categorical_features,reference = lgb_train)
+                    lgb_train = lgb.Dataset(train_X,train_y,categorical_feature = categorical_features)
+                    lgb_val = lgb.Dataset(val_X,val_y,categorical_feature = categorical_features,reference = lgb_train)
 
                     gbm = lgb.train(params = parameters,
                                     train_set=lgb_train,
                                     num_boost_round=parameters['num_boost_round'],
-                                    valid_sets=lgb_val,
+                                    valid_sets=[lgb_train,lgb_val],
                                     early_stopping_rounds=parameters['early_stopping_rounds'],
                                     evals_result = self.history,
-                                    verbose_eval = parameters['verbose_eval'])
+                                    verbose_eval = parameters['verbose_eval'],
+                                    feval = self.eval_metric)
                     valid_predictions[val_index,i] = gbm.predict(val_X,num_iteration=gbm.best_iteration)
-                    r2 = r2_score(val_y[val_index],valid_predictions[val_index,i])
-                    print(f"R squared for current validation set {r2}")
+
+                    r2= r2_score(val_y[val_index],valid_predictions[val_index,i])
+                    print(f"R2 Score for current validation set:{r2}")
+                
+                    if self.save_model:
+                        print("Saving model")
+                        gbm.save_model(f'{src_dir}/fold_{i}_{self.name}_eval_history.txt')
                     
-                if self.save_model: 
-                    print("Saving model")
-                    gbm.save_model('{0}/fold_{1}_{2}_eval_history.txt'.format(self.src_dir,i,self.name))
-                if self.save_history: 
-                    print("Saving Hisotry")
-                    pd.to_pickle(self.history,'{0}/fold_{1}_{2}_pickle_eval_history.pkl'.format(self.src_dir,i,self.name))
-                if self.importance: 
-                    self.visualize_importance(i)
-                if self.test_predict: 
-                    test_predictions[:,i] = self.predict_test(X_test,i)
-                if self.prepare_submission: 
-                    self.output_submission(self,test_predictions,i)
+                    if self.save_history: 
+                        print("Saving Hisotry")
+                        pd.to_pickle(self.history,f'{src_dir}/fold_{i}_{self.name}_pickle_eval_history.pkl')
+                    
+                    if self.test_predict: 
+                        test_predictions[:,i] = self.predict_test(X_test,i,src_dir)
+                    
+                    if self.importance: 
+                        self.visualize_importance(i,src_dir)
+                        
+                else: 
+                    print("Train XGBooost")
+                    train_X = X_train.iloc[train_index]
+                    val_X = X_train.iloc[val_index]
+                    if isinstance(y_train,pd.DataFrame): 
+                        train_y = y_train.iloc[train_index]
+                        val_y = y_train.iloc[val_index]
+                    else: 
+                        train_y = y_train[train_index]
+                        val_y = y_train[val_index]
+                    
+                    xg_train = xgb.DMatrix(train_X,label=train_y)
+                    xg_val = xgb.DMatrix(val_X,label=val_y)
+                    eval_list = [(xg_train,'train'),(xg_val,'val')]
+
+                    xgboost_train = xgb.train(parameters,xg_train,evals=eval_list,
+                                              evals_result=self.history, 
+                                              num_boost_round=parameters['boost_round'],
+                                              early_stopping_rounds=parameters['early_stopping'],
+                                              verbose_eval = parameters['verbose_eval'])
+
+                    valid_predictions[val_index,i] = xgboost_train.predict(xg_val,
+                                                                           ntree_limit = xgboost_train.best_ntree_limit)
+
+                    r2= r2_score(val_y[val_index],valid_predictions[val_index,i])
+                    print(f"R2 Score for current validation set:{r2}")
+                    
+                    if self.save_model:
+                        print("Saving model")
+                        xgboost_train.save_model(f'{src_dir}/fold_{i}_{self.name}_eval_history.txt')
+
+                    if self.save_history: 
+                        print("Saving Hisotry")
+                        pd.to_pickle(self.history,f'{src_dir}/fold_{i}_{self.name}_pickle_eval_history.pkl')
+           
+                    if self.test_predict: 
+                        test_predictions[:,i] = self.predict_test(X_test,i,src_dir,xgboost_train)
+
+                    if self.importance: 
+                        self.visualize_importance(i,src_dir)
+                        
                 i += 1
+            if self.jsonize:
+                print("Saving model parameters to json")
+                model_dict = {"model":f"{src_dir}_{i}_folds",
+                              "parameters":parameters}
+                with open(f"./parameters/{src_dir}_{i}_fold.json",'w+') as model_param: 
+                    json.dump(model_dict,model_param)
+            
+            if self.prepare_submission: 
+                self.output_submission(self,test_predictions,i)
+
             return valid_predictions,test_predictions
 
     
-    def predict_test(self,X_test,index): 
-        gbm = lgb.Booster(model_file ="{}/fold_{}_{}_eval_history.txt".format(self.src_dir,index,self.name))
-        test_predict = gbm.predict(X_test,num_iteration = gbm.best_iteration)
-        return test_predict
+    def predict_test(self,X_test,index,source_dir,xgb_model = None):
+        if self.train_gbm:
+            gbm = lgb.Booster(model_file =f"{source_dir}/fold_{index}_{self.name}_eval_history.txt")
+            test_predict = gbm.predict(X_test,num_iteration = gbm.best_iteration)
+            return test_predict
+        elif xgb_model is not None:
+            xg_mod = xgb.Booster(model_file=f"{source_dir}/fold_{index}_{self.name}_eval_history.txt")
+            test_predict = xg_mod.predict(xgb.DMatrix(X_test),ntree_limit=xgb_model.best_ntree_limit)
+            return test_predict
 
-    def visualize_importance(self,index): 
+    def visualize_importance(self,index,source_dir): 
         print("Visualize plot importance")
         if self.train_gbm:
-            gbm = lgb.Booster(model_file ="{}/fold_{}_{}_eval_history.txt".format(self.src_dir,index,self.name))
+            gbm = lgb.Booster(model_file =f"{source_dir}/fold_{index}_{self.name}_eval_history.txt")
             importance = gbm.feature_importance() 
-            names = gbm.feature_name() 
-            df_importance = pd.DataFrame() 
-            df_importance['importance'] = importance 
-            df_importance['names'] = names
-            df_importance.sort_values('importance',ascending=False,inplace=True)
-            importance_plot = df_importance.plot(x='names',y='importance',kind='barh',figsize=(10,10))
-            plt.show()
+            names = gbm.feature_name()
+        else: 
+            xg_mod = xgb.Booster(model_file=f"{source_dir}/fold_{index}_{self.name}_eval_history.txt")
+            
+            importance = list(xg_mod.get_fscore().values())
+            names = list(xg_mod.get_fscore().keys())
+        
+        df_importance = pd.DataFrame() 
+        df_importance['importance'] = importance
+        df_importance['names'] = names 
+        df_importance.sort_values('importance',ascending=False,inplace=True)
+        importance_plot = df_importance.plot(x='names',y='importance',kind='barh',figsize=(10,10))
+ 
+        if self.train_gbm: 
+            plt.title("LightGBM current validation set importance")
+        else: 
+            plt.title("XGBoost current validation set importance")
     
     def output_submission(self,predictions,index,save = True): 
         print("Preparing submission")
-        os.makedirs('submissions')
+        if src_dir:
+            if os.path.isdir(src_dir):
+                pass
+            else:
+                print(f"Making dir:{src_dir}")
+                os.makedirs(src_dir)
         submission = pd.read_csv('./data/sample_submission.csv')
         submission['predictions'] = predictions
         if save: 
             submission.to_csv('./submissions/{}_{}_.csv'.format(self.name,index))
         return submission 
-
-
-
-
-
-
-
-
-                
-                
-
-                
-
-
-
-
-
-
-                    
-
-                
-
-
-
-
-
-
-
-
-
-
-            
-
-            
-
-            
-
-
-            
-
-            
-
-
-            
-            
-
-            
-        
-            
-
